2356281491dc5cee2f902c103b32739ef8822183
iman01 <iman.esfandiyar71@gmail.com>
2023-10-21 15:13:54 +0200

    major update OK

--- launch/all_nodes.launch.py
+++ launch/all_nodes.launch.py
@@ -1,41 +1,65 @@
-import launch
-import launch_ros.actions
+from launch import LaunchDescription
+from launch_ros.actions import Node 
+from launch.actions import ExecuteProcess
+from launch.actions import DeclareLaunchArgument
+from launch.actions import IncludeLaunchDescription
+from launch_xml.launch_description_sources import XMLLaunchDescriptionSource
+from ament_index_python import get_package_share_directory
+from launch.substitutions import LaunchConfiguration
 import os
 
+import subprocess
+from getpass import getpass
+
+def check_can0_interface():
+
+    # Get network interfaces
+    result = subprocess.run(["ifconfig"], capture_output=True, text=True)
+    interfaces = result.stdout
+
+    # Check if can0 interface is present
+    if "can0" in interfaces:
+        print("can0 interface present")
+    else:
+        raise ValueError('\n can0 interface is absent, please\n1.plug-in CAN-USB adaptor\n2.run\n \n sudo ip link set can0 up type can bitrate 250000 \n \n') 
+
+
+
 def generate_launch_description():
+
+    use_sim_time = LaunchConfiguration('use_sim_time', default='false')
+
     # Get the path to the can_interface_check.py script within the package
-    package_share_dir = os.path.join(os.path.dirname(__file__), '..', 'share', 'agrorob')
-    check_script_path = os.path.join(package_share_dir, 'can_interface_check.py')
+    # package_share_dir = os.path.join(os.path.dirname(__file__), '..', 'share', 'agrorob_launch')
+    # check_script_path = os.path.join(package_share_dir, 'launch/can_interface_check.py')
 
     # Call the function to check the can0 interface
-    check_can0_interface_action = launch.actions.ExecuteProcess(
-        cmd=["python3", check_script_path],
-        output='screen'
-    )
+    # check_can0_interface_action = ExecuteProcess(
+    #     cmd=["python3", check_script_path],
+    #     output='screen'
+    # )
 
-    return launch.LaunchDescription([
-        check_can0_interface_action,
-        launch_ros.actions.Node(
-            package='ros2_socketcan',
-            executable='socket_can_receiver_node_exe',
-            name='socket_can_receiver_node'
-        ),
-        launch_ros.actions.Node(
-            package='ros2_socketcan',
-            executable='socket_can_sender_node_exe',
-            name='socket_can_sender_node'
-        ),
-        launch_ros.actions.Node(
+    return LaunchDescription([
+        check_can0_interface(),
+        DeclareLaunchArgument(
+            'use_sim_time',
+            default_value='false',
+            description='Use simulation (Gazebo) clock if true'),
+        Node(
             package='agrorob_driver',
-            executable='agrorob_driver_node',
-            name='agrorob_driver_node'
+            executable='agrorob_interface',
+            name='agrorob_interface'
         ),
-        launch_ros.actions.Node(
+        Node(
             package='joy',
             executable='joy_node',
             name='joy_node'
-        )
+        ),
+        IncludeLaunchDescription(
+            XMLLaunchDescriptionSource(
+                os.path.join(get_package_share_directory('ros2_socketcan'),
+                             'launch/socket_can_bridge.launch.xml')
+            )
+        ),
     ])
 
-if __name__ == '__main__':
-    generate_launch_description()
